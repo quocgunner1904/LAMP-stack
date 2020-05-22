42d2032847fbc3234b330a5441cbe119f9b7af17
Quoc Nguyen <quocgunner1904@knights.ucf.edu>
2020-05-22 17:30:45 -0400

    code

--- code/login.js
+++ code/login.js
@@ -40,9 +40,52 @@ function login()
 		
 		firstName = jsonObject.firstName;
 		lastName = jsonObject.lastName;
+
+		saveCookie();
 	}
 	catch(err)
 	{
 		document.getElementById("loginResult").innerHTML = err.message;
+	}
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
 }
